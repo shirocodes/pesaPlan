9cfcc1ad8c2aef9c76cb775a78db913bad68bbc8
shirocodes <ishirapt@gmail.com>
2025-03-29 13:53:09 +0300

    Initialized budget.js: handles budget creation functionality

--- src/budget.js
+++ src/budget.js
@@ -20,18 +20,23 @@ function createBudgetElement() {
     <p id="budget-error"></p>`;
 
     budgetHolder.appendChild(budgetWrap)
+    console.log("Budget input field added to the DOM.");
 
     document.getElementById("save-budgt").addEventListener("click", saveBudget)
 }
 
 //saveBudget() > input validation > saving to json > updating UI
 async function saveBudget() { 
+    console.log("Budget input field added to the DOM.");
     const budgtInput = document.getElementById("budgtInput-field")
     const budgtValue = parseInt(budgtInput.value.trim());
     budgetError.document.getElementById("budget-error")
 
+    console.log(`Entered budget value: ${budgtValue}`);
+
     //validation
     if(isNaN(budgtValue) || budgtValue <= 0) {
+        console.error("Invalid budget amount entered!");
         budgetError.textContent = "Enter a valid amount"
         budgetError.style.color = "red";
         console.warn("Invalid input")
@@ -39,16 +44,22 @@ async function saveBudget() {
     }
 
     try {
+        console.log("Fetching existing budget data...");
+
         const existingBudget = await getData("budget")
 
         if(existingBudget.length === 0) {
+            console.log("No existing budget found. Creating new budget...");
+            
             //no budget, so create one
             await postData("budget", {amount: budgtValue});
         } else {
+            console.log(`Updating existing budget (ID: ${existingBudget[0].id})...`);
+            
             //budget exists, so update it
             await pathData("budget", existingBudget[0].id, {amount: budgtValue}) 
         }
-
+        console.log("Budget successfully saved.");
         updateBudgetUI(budgtValue);
 
     } catch(error) {console.error("error saving budget:", error)}  
@@ -57,12 +68,17 @@ async function saveBudget() {
 //updating UI with saved budget ||
 //remove inputs > changing buttons color/content
 function updateBudgetUI(amount) {
+    console.log(`Updating UI with new budget: Ksh ${amount}`);
+
     const budgetWrap = document.getElementById("budget-wrapper")
+    console.log("Removing budget input container from DOM.");
+        
     if(budgetWrap) budgetWrap.remove();
 
     //update appearance of 'save Budget' butn
     budgetBTN.textContent = `Budget set: Ksh ${amount}`;
-    budgetBTN.style.backgroundColor = "green"   
+    budgetBTN.style.backgroundColor = "green"  
+    console.log("Budget button updated.") 
 }
 
 //on page load, load existing budget & updateUI
