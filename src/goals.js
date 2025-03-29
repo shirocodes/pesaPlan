4fb472b36b4d2614fb06ad107be5157a2a80487c
shirocodes <ishirapt@gmail.com>
2025-03-30 00:21:57 +0300

    Finalized goals.js: added display() & loadGoals()

--- src/goals.js
+++ src/goals.js
@@ -54,26 +54,55 @@ async function handleSaveGoal() {
 
         if(existngGoals.length === 0) {
             console.log("no existing goals found. create new ones")
-            await postData("financial_goal", {savngsValue, investmntValue})
+            await postData("financial_goal", {savings: savngsValue, investment: investmntValue})
         } else {
-            console.log(`updating existing goals (ID: ${existngGoals})`)
-            await pathData("financial_goal", existngGoals[0].id, {savngsValue, investmntValue})    
+            console.log(`updating existing goals (ID: ${existngGoals[0]})`)
+            await pathData("financial_goal", existngGoals[0].id, {savings: savngsValue, investment: investmntValue})    
         }
         console.log("goals successfully saved")
         displayGoals(savngsValue, investmntValue);
     } catch (error) {console.error("error saving goals:", error)}
 }
 
+//after saving goals > remove input form > update initial btn
+function displayGoals(savngsValue, investmntValue) {
+    console.log(`updating UI with savings: ${savngsValue} and investment: ${investmntValue}`)
+   //creating a total for savings and investment
+   const goalInputs = [savngsValue, investmntValue]
+   const totalGoals = goalInputs.reduce((sum, value) =>
+    sum + value, 0)
+   
+    const goalsWrapper = document.getElementById("goals-wrap")
+    if(goalsWrapper) {
+        console.log("removing goals input field from DOM")
+        goalsWrapper.remove()
+    }
 
+    goalsBtn.textContent = totalGoals === 0 ? "set Goals" :
+     `Goal set: Kshs ${totalGoals}`
+    goalsBtn.style.backgroundColor = totalGoals === 0 ? "white" :
+    "green";
+    console.log("goals button updated")
+}
 
+//whenever page loads, existing goals should load and get lost
+async function loadGoals() {
+    console.log("loading goals from server ...")
+    try {
+        const existngGoalsData = await getData("financial_goal")
+        console.log("fetched goals data:", existngGoalsData)
 
-
-
-
-
-
-
+        if(existngGoalsData.length > 0) {
+            displayGoals(existngGoalsData[0].savings, existngGoalsData[0].investment)
+        } else {
+            console.log("no goals found, setting to default")
+            displayGoals(0, 0);
+        }
+    }catch(error) {console.error("failed in loading goals:", error)}
+    
+}
+//on each page load, call
 document.addEventListener("DOMContentLoaded", () => {
-    // loadGoals()
+    loadGoals();
     goalsBtn.addEventListener("click", createGoalsElement)
 });
